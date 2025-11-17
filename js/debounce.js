cd4dfbf9d1aed2c9b4ca3125ac283819a34b3927
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-17 17:03:02 +0900

    js: debounce-array

--- js/debounce.js
+++ js/debounce.js
@@ -1,5 +1,4 @@
 // setTimeout(cb, delay); delay 이후에 cb 실행
-
 const debounce = (cb, delay) => {
   let timer;
   return () => {
@@ -86,3 +85,77 @@ const obj = {
 
 const t = throttleBind(obj.sayName, 200);
 t.call(obj); // 정상적으로 this = obj 유지
+
+console.log("연습문제 ---------------------------");
+
+const once = (f, rebirthDelay = 1000) => {
+  let done = false;
+  return (...args) => {
+    if (done) return;
+    done = true;
+    setTimeout(() => (done = false), rebirthDelay);
+    return f(...args);
+  };
+};
+const fn = once((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
+console.log(fn(1, 6)); // 금일 운행금지 차량은 끝번호 1, 6입니다!
+console.log(fn(2, 7)); // undefined
+console.log(fn(3, 8)); // undefined
+
+// let cnt = 0;
+// const intl = setInterval(() => console.log(cnt++, fn(cnt, -cnt)), 200);
+
+console.log("--------------------");
+const before = () => console.log("before....");
+const after = (result) => console.log("after...", result);
+
+const someFn = (name, greeting) => `${greeting}, ${name}`;
+const someFn2 = (id, nickname, email, level) =>
+  `${id}/${nickname}/${email}/${level}`;
+
+const template =
+  (f) =>
+  (...args) => {
+    before();
+    const ret = f(...args);
+    setImmediate(after, ret);
+    return ret;
+    // after();
+    // return ret;
+  };
+
+const temp = template(someFn); // before → someFn → after 실행
+const temp2 = template(someFn2); // before → someFn2 → after 실행
+
+console.log("temp1>>", temp("sico", "hello"));
+console.log("temp2>>", temp2(1, "sico", "sico@gmail.com", 5));
+
+console.log("+++++++++++++++++++++++");
+const weeks = ["일", "월", "화", "수", "목", "금", "토"];
+const getNextWeek = (() => {
+  let widx = -1;
+  return () => {
+    widx += 1; // side-effect!
+    if (widx >= weeks.length) widx = 0;
+    return `${weeks[widx]}요일`;
+  };
+})();
+
+let cnt1 = 0;
+const intl1 = setInterval(() => {
+  // widx += 2; // side-effect!
+  console.log("call", cnt1, getNextWeek());
+  if ((cnt1 += 1) === 8) clearInterval(intl1);
+}, 200);
+
+// > [...'ab,cd'.split(',')]; // 멍청이는 걸러짐.
+// [ 'ab', 'cd' ]
+// > 'ab,cd'.split(','); // 이렇게 훨씬 좋음.
+// [ 'ab', 'cd' ]
+// >
+
+// 게시글에서 찾을 떄 최신글부터 찾는게 유리하듯이..
+// lastIndexOf 좋음 !
+// arr.lastIndexOf(3);
+
+// indexOf랑 findLastIndex랑 같음
