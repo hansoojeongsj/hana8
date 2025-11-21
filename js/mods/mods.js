cfab6d15230ddca86657a8bff50d9493499527e7
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-21 14:53:23 +0900

    js: module

---
+++ js/mods/mods.js
@@ -0,0 +1,8 @@
+import x from './hangul.js';
+export { x };
+
+export { range } from './array-utils.js';
+// import { range } from './array-utils.js';
+// export { range };
+
+export * from './hangul.js';
