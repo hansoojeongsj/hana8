b9708b4a59a90da1eb10fd7965e29e584567c67c
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-02 18:30:33 +0900

    ts: jest2

--- ts/jtest/sum.ts
+++ ts/jtest/sum.ts
@@ -9,6 +9,17 @@ export const sumId = async () => {
   const users = (await fetch('https://jsonplaceholder.typicode.com/users').then(
     (res) => res.json()
   )) as User[];
+  console.log('🚀 ~ users:', users);
 
   return users.reduce((acc, { id }) => acc + id, 0);
+};
+
+export const fetchUser = async (userId: number) => {
+  const user: User = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  ).then((res) => res.json());
+  // console.log('🚀 ~ user:', user);
+  if (!user.id) throw new Error(`#${userId} User Not Found!`);
+  const { id, username } = user;
+  return { id, username };
 };
