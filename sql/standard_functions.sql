3ed098821a41f3e52b77ef8b6ed2040b74002142
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-08 14:34:28 +0900

    sql: standard functions 2

--- sql/standard_functions.sql
+++ sql/standard_functions.sql
@@ -5,7 +5,7 @@ select CAST( 1.467 AS Signed Integer ), CONVERT(1.567, Signed Integer);
 select str_to_date('2025-12-03', '%Y-%m-%d');  -- ←→ date_format  -- %ymdHis
 
 -- 한국시간
-set time_zone = 'Asia/Seoul';
+set time_zone = 'Asia/Seoul';   
 
 select date_format(now(), '%m/%d %H:%i:%s');
 
@@ -26,3 +26,153 @@ select concat(e.ename, ',', d.dname), concat_ws(',', e.ename, d.dname)
 	from Emp e
     right outer join Dept d on e.id = d.captain;
 
+select CAST(char(65, 66) as char);
+select length('AB한글'), char_length('AB한글'), bit_length('A'), sign(-2);
+-- A한글 -> 1, 3, 3 byte 
+
+select ELT(FIELD(didAgree, 'Y', 'N'), 'Active', 'Inactive');
+-- char 대신 enum, typeint ... 
+
+select elt(2, 'str1', 'str2', 'str3'), field('s1', 's0', 's1'); -- 'str2', 2
+select ELT(FIELD('N', 'Y', 'N'), 'Active', 'Inactive');
+select ELT(2, 'Active', 'Inactive');
+
+select find_in_set('s3', 's1,s2,s3,s4'); -- 3
+select dept, min(ename), group_concat(ename) from Emp group by dept;
+select dept, find_in_set(min(ename), group_concat(ename order by ename)) from Emp group by dept;
+select instr('str', 't'), locate('s1', 's0s1s2'); -- cf. indexOf
+select insert('12345', 3, 2, '/'); -- 12/5 -- (문자열, 위치, 제거수, 추가문자) splice(2, 2, '/')
+select format(123456789,0), format(78901.012356, 4) + 1, truncate(789.012356, 4);
+select left('abc', 2), right('abc', 2), lpad('5', 2, '0'), rpad('15', 3, '0');
+select reverse('abc'), repeat('x', 9), concat('|', space(5), '|'), 
+       replace('abcdefgcdeyy', 'cde', 'xxx');
+select trim(leading 's' from 'ssstrss'), ltrim('ssstrss');
+
+select substring('abcdefg', 2, 3); -- select substring('Bearer accessTokenasdafdsafdsff', 7);
+select substring_index('a,b,c', ',', 2);  -- a,b (,를 기준으로 두번째까지)
+select substring_index('a,b,c', ',', -2); -- c  (-1은 ,로 잘라낸 마지막 값)
+
+select substring_index(substring_index('a,b,c', ',', 2), ',', -1);  -- arr[1]
+
+select now(), sysdate(), curdate(), current_date(), curtime(), current_time();
+
+select year(now()), month(now()), day(now()), month('2025-11-29'),
+	   hour(now()), minute(now()), second(now()), quarter(now()), week(now());
+       
+select weekday('2025-12-25');   -- 월요일 0 ~ 6(일요일)
+select dayofweek('2025-12-25'); -- 일요일 1 ~ 7(토요일)
+
+select DATE(now()), TIME(now()), MAKEDATE(2020, 336), MAKETIME(19,3,50);
+select dayofyear(now()), monthname(now()), last_day('2025-11-01'); -- weekOfyear
+
+select datediff('2025-12-01', '2025-03-11'), timediff('12:20:33', '11:30:20');
+
+select adddate(now(), interval -31 day), subdate(now(), interval 31 day);
+select date_add(now(), interval -31 day), adddate(now(), interval -1 month);
+
+select '139' regexp '[a-z29]'; -- test(match)되면 1
+SELECT REGEXP_INSTR('dog cat dag', 'dog', 2);
+SELECT REGEXP_INSTR('aa aaa aaaa', 'a{3,4}');
+SELECT REGEXP_LIKE('abc', 'AB', 'i'); -- c: case sensitive
+SELECT REGEXP_REPLACE('abc def ghi', '[a-z ]+', 'X', 2, 1);
+-- 2번째 char(b) 부터 2번째 그룹을 X로 변경!
+
+select regexp_replace('abcdefg', '[bdf]', 'X'), 
+       regexp_replace('abcdefg', '[f-z]', 'X');
+
+--
+use schooldb;
+show create table Student;
+/*
+`gender` bit(1) NOT NULL DEFAULT b'0' COMMENT '성별(0: 남, 1: 여)',
+*/
+
+-- select id, name, birthdt, gender from Student order by ?;
+-- 위 sql문에서 성별(gender)을 '여', '남'으로 표시하시오.
+
+-- 문제만 보고 풀게된 다면, 
+select name,
+       if(gender = 1, '여', '남') as gender
+  from Student;
+
+select elt(1, '남', '여');
+select 1 + floor(3*rand());
+select id, name, birthdt, gender from Student order by id;
+
+select id, name, birthdt, gender from Student
+	order by elt(1, id, name, birthdt);
+
+-- case when 문 사용
+select id, name, birthdt, gender, 
+	case
+		when gender = 1 then '여'
+        else '남'
+	end as genderkor
+from Student;
+
+-- elt 문 사용
+-- ELT(gender + 1, '남', '여'), 0+1=1 -> 남자 
+select id, name, birthdt, gender, 
+       -- elt(field(gender, 0, 1), '남','여'),
+       elt(gender+1, '남','여')
+  from Student;
+
+-- 정렬을 id / name / birthdt 3개 컬럼 랜덤으로 수행하시오
+select id, name, birthdt, gender, 
+      (case gender when 0 then '남' else '여' end),
+      elt(field(gender, 0, 1), '남', '여'),
+      elt(gender + 1, '남', '여')
+  from Student
+ order by elt(1 + floor(3 * rand()), id, name, birthdt);
+
+-- ex) substring_index, replace, 
+-- select 'https://www.topician.com/lms/lectures'; 에서 도메인 부분만 추출하시오.
+select substring_index(
+           substring_index('https://www.topician.com/lms/lectures', '/', 3),
+           '/',
+           -1
+       ) as domain;
+-- 
+select substring_index(
+           replace('https://www.topician.com/lms/lectures', 'https://', ''),
+           '/',
+           1
+       ) as domain;
+
+-- select name from Student; 에서 name의 가운데 글자만 *로 변환해서 출력하시오.
+select name,
+       concat(
+           left(name, floor(char_length(name) / 2)),
+           '*',
+           substring(name, floor(char_length(name) / 2) + 2)
+       ) as masked_name
+  from Student;
+
+-- 다른 풀이 
+-- select name from Student; 에서 name의 가운데 글자만 *로 변환해서 출력하시오.
+select name, regexp_replace(name, '.', '*', 2, 1) from Student;
+select name, regexp_replace(name, '.{1}', '*', 2, 1) from Student;
+select name, regexp_replace(name, '.{2}', '**', 2, 1) from Student;
+
+-- 전화번호가 '010-2323-4545', '010.2323.4545', '010 2323 4545' 등 다양한 형태로 되어있다. 숫자만 추출하시오.
+select regexp_replace('010-2323-4545','[^0-9]','');
+select regexp_replace('010.2323.4545','[^0-9]','');
+select regexp_replace('010 2323 4545','[^0-9]','');
+
+-- 내가 안 해도 자동으로 된다 (?)
+select @@autocommit;
+-- commit 관련된거 다 튀어 나옴 
+show variables like '%commit';
+
+start transaction;
+-- update Student set birthdt='20250909';
+rollback;
+
+update Student set birthdt='20050909' where id =5;
+
+commit;
+select * from Student;
+
+-- altering 할 때는 백업 파일 만들기 괜찮음
+create table StudentBackup as select * from Student;
+drop table StudentBackup;
