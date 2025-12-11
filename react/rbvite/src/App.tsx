efc6720ca30617045dfdbbf2be3b5ae9b2de4b42
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-11 13:00:51 +0900

    react: jsx

---
+++ react/rbvite/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+const Hello = () => <h2>Hello</h2>;
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1 className="text-3xl">React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <Hello />
+      </div>
+    </>
+  );
+}
+
+export default App;
