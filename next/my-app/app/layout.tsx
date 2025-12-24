db511fcbaf6c4d708a0a392b33dc0aaf91cdc452
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-24 09:50:17 +0900

    next: photo gallery

--- next/my-app/app/layout.tsx
+++ next/my-app/app/layout.tsx
@@ -34,6 +34,8 @@ export default function RootLayout({
           <Link href="/hi">Hi</Link>
           <Link href="/shop/123">123</Link>
           <Link href="/shop/123/456">456</Link>
+          <Link href="/intercept">Intercept</Link>
+          <Link href="/photos">Photos</Link>
         </nav>
         <div className="border p-3">{children}</div>
         <footer className="text-center">Footer</footer>
