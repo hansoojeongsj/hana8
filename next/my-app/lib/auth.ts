b18ac7e0000ce93690e99be0e8b31cc75b96cf4a
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-29 16:03:35 +0900

    next: set up Google/GitHub OAuth with NextAuth

---
+++ next/my-app/lib/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [Google, Github],
+  // secret: process.env.AUTH_SECRET as string,
+});
