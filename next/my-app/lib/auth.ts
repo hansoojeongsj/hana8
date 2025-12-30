9953d815c3150634474fa3e58c551f26eebedac5
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-30 13:51:17 +0900

    next: credentials

--- next/my-app/lib/auth.ts
+++ next/my-app/lib/auth.ts
@@ -22,14 +22,39 @@ export const {
       },
       async authorize(credentials) {
         console.log('🚀 ~ credentials:', credentials);
-        const { email } = credentials;
-        return { id: '1', email: email as string, name: 'HONG' };
+        const { email, passwd } = credentials;
+        return { id: '1', email: email as string, name: 'HONG', passwd };
       },
     }),
     Google,
     Github,
   ],
-  callbacks: {},
+  callbacks: {
+    async signIn({ profile, user }) {
+      console.log('🚀 signIn - profile:', profile);
+      console.log('🚀 signIn - user:', user);
+      return true;
+    },
+    async jwt({ token, user, trigger }) {
+      console.log('🚀 jwt - token:', token);
+      console.log('🚀 jwt - user:', user);
+      console.log('🚀 jwt - trigger:', trigger);
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, user }) {
+      if (user) {
+        session.user.id = user.id;
+        session.user.email = user.email;
+        session.user.name = user.name;
+      }
+      return session;
+    },
+  },
   pages: {
     // signIn: '/sign',
     error: '/sign/error',
@@ -37,4 +62,6 @@ export const {
   session: {
     strategy: 'jwt',
   },
+  trustHost: true,
+  jwt: { maxAge: 30 * 60 },
 });
