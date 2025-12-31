7ca5207703cc6e58f2cbb0b4dd27dc3bc21e2248
hansoojeongsj <tnwjd3758@gmail.com>
2025-12-31 11:00:25 +0900

    next: reinitialize prisma config, migrations and seed

---
+++ next/my-app/prisma/seed.ts
@@ -0,0 +1,49 @@
+import { prisma } from '@/lib/prisma';
+
+console.log('SEED DATABASE_URL =', process.env.DATABASE_URL);
+
+async function main() {
+  const folder = await prisma.folder.upsert({
+    where: { title: 'JS/TS' },
+    update: {},
+    create: { title: 'JS/TS' },
+  });
+
+  for (const { name, title } of data) {
+    const email = `${name}@email.com`;
+
+    const rs = await prisma.user.upsert({
+      where: { email },
+      update: {},
+      create: {
+        email,
+        name,
+        Post: {
+          create: { folder: folder.id, title, content: title },
+        },
+      },
+    });
+    console.log('🚀 ~ rs:', rs);
+  }
+}
+
+const data = [
+  {
+    name: 'hong',
+    title: 'post1',
+  },
+  {
+    name: 'kim',
+    title: 'post2',
+  },
+];
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error('PrismaError>>', e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
