3d5b7e46c0c1891f781811983099617648e0170f
hansoojeongsj <tnwjd3758@gmail.com>
2026-02-09 17:44:53 +0900

    java: prac 변수 기초

---
+++ java/hello/src/main/java/prac/variable/Var4.java
@@ -0,0 +1,22 @@
+package prac.variable;
+
+public class Var4 {
+	public static void main(String[] args) {
+		int a = 10; // 정수, 정수 리터럴
+		double b = 10.5; // 실수, 실수 리터럴
+		boolean c = true; // 불리언 t/f, 불리언 리터럴
+		char d = 'a'; // 문자 딱 하나, 문자 하나 리터럴
+		String e = "Hello JAVA"; // 문자열, 다루기 위한 특별한 타입, 문자열 리터럴
+		// String은 문자 길이에 따라 동적으로 메모리 사용량이 달라짐
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+
+		// 코드에서 개발자가 직접 적은 100, 10.5, true, 'a' 등 고정된 값을
+		// 프로그래밍 용어로 리터럴(literal)이라고 함
+		// '리터럴'이라는 단어의 어원이 문자 또는 글자를 의미함
+	}
+}
