fc3c4c9b936721ad04d5f1faffc35121cfe899e6
hansoojeongsj <tnwjd3758@gmail.com>
2026-02-26 14:25:40 +0900

    java: annotation - bean,Qulifier,primary,bean,lazy,scope

--- java/demo/src/main/java/com/hana8/demo/DemoApplication.java
+++ java/demo/src/main/java/com/hana8/demo/DemoApplication.java
@@ -2,17 +2,49 @@ package com.hana8.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import com.hana8.demo.controller.GreetingController;
+import com.hana8.demo.controller.HelloController;
+import com.hana8.demo.service.EagerCallService;
+import com.hana8.demo.service.HelloService;
+import com.hana8.demo.service.HelpCallService;
+import com.hana8.demo.service.LazyCallService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	// @SpringBootApplication(@Component + @Bean + @ComponentScan)
-	// main이 다 구동되면서, @Component + @Bean 붙은 애들 다 등록했음.
-	// @Autowired : 스프링에게 Injection 좀 해줘 ~
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 
-	// ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-	// ConfigurableApplicationContext
+		HelloController helloBean = ctx.getBean(HelloController.class);
+		log.debug("hello = {}", helloBean.hello());
+
+		HelloService helloService = ctx.getBean(HelloService.class);
+		log.debug("helloService = {}", helloService.sayHello());
+		HelloService helloService2 = (HelloService)ctx.getBean("hello-service");
+		log.debug("helloService2 = {}", helloService2.sayHello());
+
+		// GreetingService greetingService = ctx.getBean(GreetingService.class);
+		// GreetingService greetingService = ctx.getBean(HelloCallService.class);
+		GreetingController greetingService = ctx.getBean(GreetingController.class);
+		log.debug("greetingService = {}", greetingService.call());
+
+		HelpCallService help = ctx.getBean(HelpCallService.class);
+		log.debug("help = {}", help.call());
+
+		System.out.println(
+			"ctx.getBean(EagerCallService.class).hashCode() = " + ctx.getBean(EagerCallService.class).hashCode());
+		System.out.println(
+			"ctx.getBean(EagerCallService.class).hashCode() = " + ctx.getBean(EagerCallService.class).hashCode());
+		System.out.println(
+			"ctx.getBean(EagerCallService.class).hashCode() = " + ctx.getBean(EagerCallService.class).hashCode());
+
+		LazyCallService lazy = ctx.getBean(LazyCallService.class);
+		log.debug("lazy = {}", lazy.call());
+	}
+
 }
